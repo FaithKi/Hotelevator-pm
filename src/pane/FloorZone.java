7b74e023eb93bba0b7a5c9f159821566d0d446d3
JeansAthiwat <athiwat5146@gmail.com>
2023-05-16 19:52:11 +0700

    add floor to floorzone

--- src/pane/FloorZone.java
+++ src/pane/FloorZone.java
@@ -9,27 +9,40 @@ import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.BackgroundRepeat;
 import javafx.scene.layout.BackgroundImage;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import sharedObject.RenderableHolder;
 import utils.Config;
 
-public class FloorZone extends GridPane {
-	private Hotel hotel = new Hotel();
+public class FloorZone extends VBox {
+	private Hotel hotel;
 	private ArrayList<FloorPane> floorsPane;
 
 	public FloorZone() {
+		initializeFloorZoneStyle();
+
+		hotel = new Hotel();
+		this.floorsPane = new ArrayList<>();
+
+		ArrayList<ArrayList<BaseCustomer>> hotelFloors = hotel.getFloors();
+		for (int i = 1; i < hotelFloors.size() + 1; i++) {
+			ArrayList<BaseCustomer> hotelFloor = hotelFloors.get(i - 1); // arrayList<BaseCustomer>
+			FloorPane currentFloorPane = new FloorPane(i, hotelFloor);
+			floorsPane.add(currentFloorPane);
+		}
+
+		this.getChildren().addAll(floorsPane);
+	}
+
+	public void InitializeFloorPane() {
+
+	}
+
+	public void initializeFloorZoneStyle() {
 		this.setPrefSize((Config.UNIT * 8.5), (Config.UNIT * (1.125 * 7)));
 //		this.setBackground(new Background(new BackgroundFill(Color.RED, null, null)));
 		this.setBackground(new Background(new BackgroundImage(RenderableHolder.hotelSprite, BackgroundRepeat.NO_REPEAT,
 				BackgroundRepeat.NO_REPEAT, null, null)));
-		ArrayList<ArrayList<BaseCustomer>> hotelFloors = hotel.getFloors();
-		for (int i = 1; i < hotelFloors.size()+1; i++) {
-//			hotelFloors.get(i)
-			
-		}
-	}
-
-	public void InitializeFloorPane() {
 
 	}
 }
