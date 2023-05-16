57f4d5fc1c69ada5f10b9bc7afb7a173fb53d355
JeansAthiwat <athiwat5146@gmail.com>
2023-05-16 15:21:57 +0700

    add template to main

--- src/main/main.java
+++ src/main/main.java
@@ -1,17 +1,32 @@
 package main;
 
+import utils.Config;
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
-public class main extends Application{
-
+public class main extends Application {
 
 	@Override
-	public void start(Stage mainStage) throws Exception {
-		
+	public void start(Stage stage) throws Exception {
+		Image backgroundImage = new Image(ClassLoader.getSystemResource("gameBackgroundImage.png").toString(),
+				Config.SCREEN_WIDTH, Config.SCREEN_HEIGHT, true, false);
+		ImageView bgImageView = new ImageView(backgroundImage);
+		StackPane root = new StackPane();
+		root.getChildren().add(bgImageView);
+
+		Scene scene = new Scene(root, Config.SCREEN_WIDTH, Config.SCREEN_HEIGHT);
+
+		stage.setScene(scene);
+		stage.setTitle("Elevator TheGame");
+		stage.setResizable(false);
+		stage.show();
 	}
 
-	
 	public static void main(String[] args) {
 		launch();
 	}
--- src/utils/Config.java
+++ src/utils/Config.java
@@ -1,5 +1,8 @@
 package utils;
 
 public class Config {
+	public static final double SCREEN_WIDTH = 1440;
+	public static final double SCREEN_HEIGHT = 810;
+	public static final double UNIT = (SCREEN_WIDTH / 16); // 16:9
 
 }
