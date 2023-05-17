3c825666bec5f625c1f2f8f8975ea986119aae3c
JeansAthiwat <athiwat5146@gmail.com>
2023-05-17 14:42:08 +0700

    feat: add clickable Pane for each Cstmer

--- src/pane/CustomerPane.java
+++ src/pane/CustomerPane.java
@@ -1,7 +1,65 @@
 package pane;
 
+import java.util.ArrayList;
+
+import javafx.event.Event;
+import javafx.event.EventHandler;
+import javafx.scene.Cursor;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.Border;
+import javafx.scene.layout.BorderStroke;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import utils.Config;
 
 public class CustomerPane extends GridPane {
+	private int index;
+	private int floorNumber;
+
+	public CustomerPane(int index, int floorNumber) {
+		this.index = index;
+		this.floorNumber = floorNumber;
+		initializeCustomerPaneStyle();
+		addEventListener();
+	}
+
+	public ArrayList<Integer> getPos() {
+		ArrayList<Integer> pos = new ArrayList<Integer>();
+		pos.add(getIndex());
+		pos.add(getFloorNumber());
+		return pos;
+	}
+
+	public int getIndex() {
+		return index;
+	}
+
+	public void setIndex(int index) {
+		this.index = index;
+	}
+
+	public int getFloorNumber() {
+		return floorNumber;
+	}
+
+	public void setFloorNumber(int floorNumber) {
+		this.floorNumber = floorNumber;
+	}
+
+	public void addEventListener() {
+		this.setOnMouseClicked((e) -> {
+			System.out.println(getPos().toString());
+			System.out.println("what the hellllll");
+		});
+	}
+
+	public void initializeCustomerPaneStyle() {
+		this.setPrefSize(Config.UNIT * 0.75, Config.UNIT * 1.125);
+//		this.setBackground(new Background(new BackgroundFill(Color.BEIGE, null, null)));
+		this.setBorder(new Border(new BorderStroke(Color.BLACK, null, null, null)));
+	}
 
 }
--- src/pane/FloorPane.java
+++ src/pane/FloorPane.java
@@ -3,6 +3,7 @@ package pane;
 import java.util.ArrayList;
 
 import entity.cutomer.BaseCustomer;
+import javafx.geometry.Insets;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.BackgroundImage;
@@ -23,12 +24,23 @@ public class FloorPane extends HBox {
 
 		this.floorNumber = floorNumber;
 
+		this.customerPanes = new ArrayList<CustomerPane>();
+
+		for (int i = 0; i < Config.MAX_CUSTOMER_PER_FLOOR; i++) {
+			CustomerPane customerPane = new CustomerPane(i, floorNumber);
+			customerPanes.add(customerPane);
+		}
+		this.getChildren().addAll(customerPanes);
+
 	}
 
 	public void initializeFloorPaneStyle() {
 		this.setPrefSize(Config.UNIT * 8.5, Config.UNIT * (1.125));
 //		this.setBackground(new Background(new BackgroundFill(Color.RED, null, null)));
-		this.setBackground(new Background(new BackgroundImage(RenderableHolder.hotelFloorSprite,
-				BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, null, null)));
+//		this.setBackground(new Background(new BackgroundImage(RenderableHolder.hotelFloorSprite,
+//				BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, null, null)));
+		this.setSpacing(Config.UNIT * 0.05);
+		this.setPadding(new Insets(0, Config.UNIT * 0.2, 0, 0));
 	}
+
 }
