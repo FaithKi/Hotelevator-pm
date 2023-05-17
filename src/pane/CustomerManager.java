9724310334f297f4fe30be91e74725058f89467f
Pornpipat Kingchongcharoensuk <faith.kingchongcharoensuk@gmail.com>
2023-05-17 15:03:41 +0700

    re;ghdfs

--- src/pane/CustomerManager.java
+++ src/pane/CustomerManager.java
@@ -1,9 +1,12 @@
 package pane;
 
 import javafx.scene.layout.HBox;
+import logic.game.GameLogic;
 
 public class CustomerManager extends HBox{
-
+	
+	private final GameLogic logic = GameLogic.getInstance();
+	
 	public CustomerManager() {
 		
 	}
