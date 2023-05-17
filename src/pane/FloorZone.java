fd9e485241465bfa72e79d980bf10adce87566a3
Pornpipat Kingchongcharoensuk <faith.kingchongcharoensuk@gmail.com>
2023-05-18 02:48:55 +0700

    sdgs

--- src/pane/FloorZone.java
+++ src/pane/FloorZone.java
@@ -2,71 +2,18 @@ package pane;
 
 import java.util.ArrayList;
 
-import entity.building.CustomerGrid;
-import entity.cutomer.BaseCustomer;
-import javafx.geometry.Insets;
-import javafx.scene.canvas.Canvas;
-import javafx.scene.canvas.GraphicsContext;
-import javafx.scene.layout.Background;
-import javafx.scene.layout.BackgroundFill;
-import javafx.scene.layout.BackgroundRepeat;
-import javafx.scene.layout.BackgroundImage;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.StackPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
-import javafx.scene.paint.Color;
-import logic.game.GameLogic;
-import sharedObject.RenderableHolder;
 import utils.Config;
 
-public class FloorZone extends GridPane {
-	private CustomerGrid hotel;
-	private VBox hotelPane;
-	private Canvas hotelCanvas;
-	private GraphicsContext gc;
-
+public class FloorZone extends VBox {
+	private ArrayList<HBox> floors;
+	
 	public FloorZone() {
-		hotel = GameLogic.getInstance().getHotel(); // getHotelInstance from gameLOgic
-		initializeCanvas();
-		initializeFloorZoneStyle();
-		InitializeFloorPane();
-	}
-
-	public void InitializeFloorPane() {
-
-		this.hotelPane = new VBox();
-		hotelPane.setPrefSize((Config.UNIT * 8.5), (Config.UNIT * (1.125 * 7)));
-
-		ArrayList<FloorPane> floorsPane = new ArrayList<>();
-		BaseCustomer[][] hotelFloors = hotel.getFloors();
-		for (int i = hotelFloors[0].length - 1; i >= 0; i--) {
-			FloorPane currentFloorPane = new FloorPane(i);
-			floorsPane.add(currentFloorPane);
+		super((Config.UNIT * 1.5), (Config.UNIT * (1.125 * 7)));
+		floors = new ArrayList<>();
+		for(int i = 0; i < 7; i++) {
+			floors.add(new HBox());
 		}
-
-		hotelPane.getChildren().addAll(floorsPane);
-
-		this.add(hotelPane, 0, 0);
 	}
-
-	public void initializeFloorZoneStyle() {
-		this.setPrefSize((Config.UNIT * 8.5), (Config.UNIT * (1.125 * 7)));
-		this.setBackground(new Background(new BackgroundImage(RenderableHolder.hotelSprite, BackgroundRepeat.NO_REPEAT,
-				BackgroundRepeat.NO_REPEAT, null, null)));
-
-	}
-
-	public void initializeCanvas() {
-		this.hotelCanvas = new Canvas(Config.UNIT * (8.5), Config.UNIT * (7 * 1.125));
-		this.gc = hotelCanvas.getGraphicsContext2D();
-
-		this.add(hotelCanvas, 0, 0);
-		gc.setFill(Color.BLACK);
-		gc.strokeRect(0, 20, 0.2 * Config.UNIT, 5);
-	}
-
-	public GraphicsContext getGc() {
-		return gc;
-	}
-
 }
