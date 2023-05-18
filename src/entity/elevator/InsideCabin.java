04ea47e8648b8093a9092785d782d9acbfa522f6
JeansAthiwat <athiwat5146@gmail.com>
2023-05-18 22:30:38 +0700

    fix:font

--- src/entity/elevator/InsideCabin.java
+++ src/entity/elevator/InsideCabin.java
@@ -10,6 +10,7 @@ import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import logic.game.GameLogic;
+import sharedObject.RenderableHolder;
 import utils.Config;
 import utils.CustomerUtils;
 
@@ -30,8 +31,9 @@ public class InsideCabin extends Entity {
 		// TODO Auto-generated method stub
 		gc.clearRect(0, 0, gc.getCanvas().getWidth(), gc.getCanvas().getHeight());
 		gc.setFill(Color.BLACK);
-		gc.setFont(Font.font("Times New Roman", FontWeight.EXTRA_BOLD,80));
-		gc.fillText(Integer.toString(this.getElevator().getId()), 0.45*Config.UNIT,0.9*Config.UNIT);
+//		gc.setFont(Font.font("Times New Roman", FontWeight.EXTRA_BOLD,80));
+		gc.setFont(Font.font(RenderableHolder.pixelStyleFont.getFamily(), 80));
+		gc.fillText(Integer.toString(this.getElevator().getId()), 0.2 * Config.UNIT, Config.UNIT);
 		for (int i = 0; i < passengers.length - 1; i++) {
 			if (!(passengers[i] == null))
 				passengers[i].drawInCabin(gc);
@@ -46,7 +48,9 @@ public class InsideCabin extends Entity {
 				customer.update();
 				if (customer.getPatienceLeft() == 0) {
 					CustomerUtils.removeCustomerFromCabin(customer, this, i);
-					GameLogic.getInstance().getTimeGauge().setTimeLeft(GameLogic.getInstance().getTimeGauge().getTimeLeft() - (int) Math.round(Config.MAX_TIME_GAUGE*0.05));
+					GameLogic.getInstance().getTimeGauge()
+							.setTimeLeft(GameLogic.getInstance().getTimeGauge().getTimeLeft()
+									- (int) Math.round(Config.MAX_TIME_GAUGE * 0.05));
 				}
 			}
 
