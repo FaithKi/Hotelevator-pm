eb3b21d3c4a3579073260f86f22f5a155a87f9f4
JeansAthiwat <athiwat5146@gmail.com>
2023-05-19 12:45:03 +0700

    chatGPT is better than me at this

--- src/pane/FloorZone.java
+++ src/pane/FloorZone.java
@@ -1,9 +1,5 @@
 package pane;
 
-import java.util.ArrayList;
-
-import entity.building.CustomerGrid;
-import entity.cutomer.BaseCustomer;
 import input.InputUtility;
 import javafx.geometry.Insets;
 import javafx.scene.Cursor;
@@ -19,10 +15,8 @@ import javafx.scene.layout.BorderWidths;
 import javafx.scene.layout.BackgroundImage;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
-import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
-import logic.game.GameLogic;
 import sharedObject.RenderableHolder;
 import utils.Config;
 
@@ -38,15 +32,10 @@ public class FloorZone extends GridPane {
 
 	public void initializeClickableGridGetterPanes() {
 		VBox floorPanes = new VBox();
-		floorPanes.setPrefSize((Config.UNIT * 8.5), (Config.UNIT * (7 * 1.125)));
+		floorPanes.setPrefSize(Config.UNIT * 8.5, Config.UNIT * (7 * 1.125));
 
 		for (int floor = Config.TOP_FLOOR; floor >= 0; floor--) {
-
-			HBox floorPane = new HBox();
-			floorPane.setPrefSize(Config.UNIT * 8.5, Config.UNIT * (1.125));
-			floorPane.setSpacing(Config.UNIT * 0.05);
-			floorPane.setPadding(new Insets(0, 0, 0, Config.UNIT * 0.2));
-
+			HBox floorPane = createFloorPane();
 			for (int queue = 0; queue < Config.MAX_CUSTOMER_PER_FLOOR; queue++) {
 				GridPane customerGridPane = createCustomerPane(queue, floor);
 				floorPane.getChildren().add(customerGridPane);
@@ -57,6 +46,14 @@ public class FloorZone extends GridPane {
 		this.add(floorPanes, 0, 0);
 	}
 
+	private HBox createFloorPane() {
+		HBox floorPane = new HBox();
+		floorPane.setPrefSize(Config.UNIT * 8.5, Config.UNIT * 1.125);
+		floorPane.setSpacing(Config.UNIT * 0.05);
+		floorPane.setPadding(new Insets(0, 0, 0, Config.UNIT * 0.2));
+		return floorPane;
+	}
+
 	private GridPane createCustomerPane(int queue, int floor) {
 		GridPane customerGridPane = new GridPane();
 
@@ -64,26 +61,30 @@ public class FloorZone extends GridPane {
 		customerGridPane.setBorder(new Border(
 				new BorderStroke(Color.BLACK, BorderStrokeStyle.SOLID, null, new BorderWidths(2)), null, null));
 
-		customerGridPane.setOnMouseClicked((e) -> {
-			Integer[] pos = { queue, floor };
-			InputUtility.setHotelGridPressed(pos, true);
-		});
-
-		customerGridPane.setOnMouseEntered(event -> {
-			customerGridPane.setCursor(Cursor.HAND);
-		});
-
-		customerGridPane.setOnMouseExited(event -> {
-			customerGridPane.setCursor(Cursor.DEFAULT);
-		});
+		customerGridPane.setOnMouseClicked(e -> handleCustomerPaneClicked(queue, floor));
+		customerGridPane.setOnMouseEntered(event -> setCursorToHand(customerGridPane));
+		customerGridPane.setOnMouseExited(event -> setCursorToDefault(customerGridPane));
 
 		return customerGridPane;
 	}
 
+	private void handleCustomerPaneClicked(int queue, int floor) {
+		Integer[] pos = { queue, floor };
+		InputUtility.setHotelGridPressed(pos, true);
+	}
+
+	private void setCursorToHand(GridPane customerGridPane) {
+		customerGridPane.setCursor(Cursor.HAND);
+	}
+
+	private void setCursorToDefault(GridPane customerGridPane) {
+		customerGridPane.setCursor(Cursor.DEFAULT);
+	}
+
 	public void initializeFloorZoneStyle() {
-		this.setPrefSize((Config.UNIT * 8.5), (Config.UNIT * (1.125 * 7)));
-		this.setBackground(new Background(new BackgroundImage(RenderableHolder.hotelSprite, BackgroundRepeat.NO_REPEAT,
-				BackgroundRepeat.NO_REPEAT, null, null)));
+		this.setPrefSize(Config.UNIT * 8.5, Config.UNIT * (1.125 * 7));
+//		this.setBackground(new Background(new BackgroundImage(RenderableHolder.hotelSprite, BackgroundRepeat.NO_REPEAT,
+//				BackgroundRepeat.NO_REPEAT, null, null)));
 
 	}
 
