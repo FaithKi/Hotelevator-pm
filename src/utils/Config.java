cd96b27efc045c13e1e02842297ec3ee447c1a68
JeansAthiwat <athiwat5146@gmail.com>
2023-05-21 00:35:42 +0700

    DONE BITCVH:

--- src/utils/Config.java
+++ src/utils/Config.java
@@ -4,15 +4,13 @@ import javafx.scene.paint.Color;
 import javafx.scene.paint.Paint;
 
 public class Config {
-//	public static final double SCREEN_WIDTH = 1440;
-//	public static final double SCREEN_HEIGHT = 810;
 	public static final double SCREEN_WIDTH = 1280;
 	public static final double SCREEN_HEIGHT = 720;
 	public static final double UNIT = (SCREEN_WIDTH / 16); // 16:9
 	public static final double FLOOR_HEIGHT = UNIT * 1.125;
 
 	public static final int DECREASE_RATE = 20;
-	public static final double MAX_PATIENCE = 70000;
+	public static final double MAX_PATIENCE = 100000;
 	public static final double MAX_LOW_PATIENCE = MAX_PATIENCE * 0.5;
 	public static final double MAX_MEDIUM_PATIENCE = MAX_PATIENCE;
 	public static final double MAX_HIGH_PATIENCE = MAX_PATIENCE * 1.3;
@@ -29,7 +27,7 @@ public class Config {
 	public static final int MAX_CUSTOMER_PER_FLOOR = 10;
 	public static final int MAX_CUSTOMER_PER_CABIN = 5;
 
-	public static final int MAX_TIME_GAUGE = 200;
+	public static final int MAX_TIME_GAUGE = 10000;
 	public static final int BASE_SCORE_GAIN = 100;
 	public static final double TIME_GAIN = MAX_TIME_GAUGE * 0.025;
 	public static final double TIME_LOSS = MAX_TIME_GAUGE * 0.05;
