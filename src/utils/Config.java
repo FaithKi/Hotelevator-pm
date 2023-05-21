8cc094addb008c6d5f44c174a87d9342eace5630
JeansAthiwat <athiwat5146@gmail.com>
2023-05-21 18:26:03 +0700

    commit

--- src/utils/Config.java
+++ src/utils/Config.java
@@ -11,7 +11,7 @@ public class Config {
 	public static final double UNIT = (SCREEN_WIDTH / 16); // 16:9
 	public static final double FLOOR_HEIGHT = UNIT * 1.125;
 
-	public static final int DECREASE_RATE = 20;
+	public static final int DECREASE_RATE = 10;
 	public static final double MAX_PATIENCE = 100000;
 	public static final double MAX_LOW_PATIENCE = MAX_PATIENCE * 0.5;
 	public static final double MAX_MEDIUM_PATIENCE = MAX_PATIENCE;
@@ -29,7 +29,7 @@ public class Config {
 	public static final int MAX_CUSTOMER_PER_FLOOR = 10;
 	public static final int MAX_CUSTOMER_PER_CABIN = 5;
 
-	public static final int MAX_TIME_GAUGE = 10000;
+	public static final int MAX_TIME_GAUGE = 15000;
 	public static final int BASE_SCORE_GAIN = 100;
 	public static final double TIME_GAIN = MAX_TIME_GAUGE * 0.025;
 	public static final double TIME_LOSS = MAX_TIME_GAUGE * 0.05;
@@ -45,6 +45,7 @@ public class Config {
 	public static final Color TEXT_FILL = Color.WHITE;
 	public static final Color TEXT_STROKE = Color.BLACK;
 
-	public static final double ELEV_SPEED = UNIT * 0.0225;
+//	public static final double ELEV_SPEED = UNIT * 0.0225;
+	public static final double ELEV_SPEED = UNIT * 0.0125;
 
 }
